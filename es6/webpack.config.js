2f6443403f39350cc1f6b13c4ce3d8108571cebe
newming <a977527479@sina.com>
2016-11-12 16:34:52 +0800

    aaa

--- es6/webpack.config.js
+++ es6/webpack.config.js
@@ -1,9 +1,18 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: path.resolve(__dirname, 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'build'),
+        // path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    }
 };
