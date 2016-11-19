802da99eb88eae74739e304c1147d9e736b9c490
newming <a977527479@sina.com>
2016-11-19 16:27:14 +0800

    state

--- react-11-19/src/components/Header.js
+++ react-11-19/src/components/Header.js
@@ -3,11 +3,25 @@ import React from 'react';
 import avater from '../images/avater.jpg';
 
 class Header extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      show:false,
+      hello: 0
+    }
+  }
+  handleClick(){
+    // console.log(this);
+    this.setState({
+      hello:this.state.hello + 1
+    })
+  }
   render(){
+    console.log(this.state.hello);
     let styles = {
       ul:{
         listStyle:'none',
-        padding:'0'
+        padding:'0',
       },
       li:{
         display:'inline-block',
@@ -16,15 +30,15 @@ class Header extends React.Component{
     }
     return (
       <div>
-        <h1>get Bootstrop</h1>
+        {this.state.hello}
+        <h1 onClick={this.handleClick.bind(this)}>get Bootstrop</h1>
         <ul style={styles.ul} className="menu">
-          <li style={styles.li}>Bootstrop</li>
+          <li style={styles.li}>{this.state.show}</li>
           <li style={styles.li}>get start</li>
           <li style={styles.li}>CSS</li>
           <li style={styles.li}>Componets</li>
-          <li style={styles.li}>Javascript</li>
+          <li style={styles.li}>{this.state.show ? '我现在是显示' : '我现在是隐藏'}</li>
         </ul>
-        <img src={avater} alt='avater'/>
       </div>
     )
   }
