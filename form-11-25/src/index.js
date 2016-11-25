684313bd81df1eaf10186a4f9dfb322433512961
newming <a977527479@sina.com>
2016-11-25 11:35:20 +0800

    form

---
+++ form-11-25/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import Forms from './Forms';
+
+ReactDOM.render(<Forms />,document.getElementById('root'));
