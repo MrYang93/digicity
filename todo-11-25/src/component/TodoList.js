311eaf3f6393797ceb7208450dcb826df58942ef
newming <a977527479@sina.com>
2016-11-26 10:42:41 +0800

    del

--- todo-11-25/src/component/TodoList.js
+++ todo-11-25/src/component/TodoList.js
@@ -5,12 +5,15 @@ class TodoList extends React.Component{
     // alert(i)
     this.props.handleCompleted(i);
   }
+  handleDelete(i){
+    this.props.handleDel(i)
+  }
   render(){
     let list = this.props.items.map( (item,index) =>
       <div key={Math.random()}>
         <input type='radio' checked={item.completed} onChange={this.handleChange.bind(this,index)}/>
         <span style={item.completed ? {textDecoration: 'line-through',opacity:'0.6'} : null}>{item.title}</span>
-        <button>删除</button>
+        <button onClick={this.handleDelete.bind(this,index)}>删除</button>
       </div>
     )
     return(
