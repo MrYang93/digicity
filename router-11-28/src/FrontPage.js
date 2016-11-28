235e3ee7c9183cadbdb42b448631b8ba36b0d0a6
newming <a977527479@sina.com>
2016-11-28 14:31:13 +0800

    router

--- router-11-28/src/FrontPage.js
+++ router-11-28/src/FrontPage.js
@@ -3,8 +3,12 @@ import React from 'react';
 class About extends React.Component{
   render(){
     return(
-      <div>
-        我是首页的内容<br />
+      <div className="jumbotron">
+        <div className="container">
+          <h1>Hello, world!</h1>
+          <p>这是我的首页，欢迎光临</p>
+          <p><a className="btn btn-primary btn-lg" role="button">Learn more</a></p>
+        </div>  
       </div>
     )
   }
